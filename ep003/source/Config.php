69203278f30bcc49ed5fb929c928c04eee30c8d4
hector-vs <hectorvieirasaldivar@gmail.com>
2023-10-18 21:57:55 -0300

    update ep003

---
+++ ep003/source/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("MAIL", [
+    "host" => "",
+    "port" => "",
+    "user" => "",
+    "passwd" => "",
+    "from_name" => "Hector V. Saldivar",
+    "from_email" => "hectorvieirasaldivar@gmail.com"
+]);
---
+++ ep003/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hector-vs/phptips003',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e61dba3cf1d12d2b7aa34e7d140ad37d4c4103a2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hector-vs/phptips003' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e61dba3cf1d12d2b7aa34e7d140ad37d4c4103a2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.0.7',
+            'version' => '6.0.7.0',
+            'reference' => '0c41a36d4508d470e376498c1c0c527aa36a2d59',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
